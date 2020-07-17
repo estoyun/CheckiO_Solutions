984cff06d55c4d7474c0895a1aecbde4d7f5eec6
PyeongYun Myeong <estoyun@kakao.com>
2020-07-17 13:22:47 +0900

    Create MaxDigit.py

---
+++ ELEMENTARY/MaxDigit.py
@@ -0,0 +1,24 @@
+def max_digit(number: int) -> int:
+    # your code here
+
+    # # 1. 기본적인 답
+    # maxValue = -1
+    # for i in str(number):
+    #     maxValue = max(int(i),maxValue)
+    # return maxValue
+
+    # 2. one-liner
+    # String max() 활용
+    return int(max(str(number)))
+
+if __name__ == '__main__':
+    print("Example:")
+    print(max_digit(0))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert max_digit(0) == 0
+    assert max_digit(52) == 5
+    assert max_digit(634) == 6
+    assert max_digit(1) == 1
+    assert max_digit(10000) == 1
+    print("Coding complete? Click 'Check' to earn cool rewards!")
