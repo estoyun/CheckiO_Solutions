6bf31774b8d7799f141c69815f23f715ab55b208
PyeongYun Myeong <estoyun@kakao.com>
2020-07-21 18:38:45 +0900

    Create NearestValue.py

---
+++ ELEMENTARY+/NearestValue.py
@@ -0,0 +1,36 @@
+'''
+[A few clarifications]
+If 2 numbers are at the same distance, you need to choose the smallest one;
+The set of numbers is always non-empty, i.e. the size is >=1;
+The given value can be in this set, which means that it’s the answer;
+The set can contain both positive and negative numbers, but they are always integers;
+The set isn’t sorted and consists of unique numbers.
+'''
+
+def nearest_value(values: set, one: int) -> int:
+    
+    # 1. 값을 조금씩 증감해가며 노가다로 찾기
+    if one in values:
+        return one
+    n = 1
+    while True:
+        if (one - n) in values:
+            return one - n
+        elif (one + n) in values:
+            return one + n
+        n += 1
+
+if __name__ == '__main__':
+    print("Example:")
+    print(nearest_value({4, 7, 10, 11, 12, 17}, 9))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 9) == 10
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 8) == 7
+    assert nearest_value({4, 8, 10, 11, 12, 17}, 9) == 8
+    assert nearest_value({4, 9, 10, 11, 12, 17}, 9) == 9
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 0) == 4
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 100) == 17
+    assert nearest_value({5, 10, 8, 12, 89, 100}, 7) == 8
+    assert nearest_value({-1, 2, 3}, 0) == -1
+    print("Coding complete? Click 'Check' to earn cool rewards!")
