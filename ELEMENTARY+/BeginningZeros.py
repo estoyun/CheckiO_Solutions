e85f524a896b1622772ed2c2b77761c40c4e0ba7
PyeongYun Myeong <audvuddbs@gmail.com>
2020-08-04 13:13:53 +0900

    Update BeginningZeros.py

--- ELEMENTARY+/BeginningZeros.py
+++ ELEMENTARY+/BeginningZeros.py
@@ -1,5 +1,9 @@
 def beginning_zeros(number: str) -> int:
     # your code here
+    
+    # strip([chars]) : 인자로 전달된 문자를 String의 왼쪽과 오른쪽에서 제거합니다.
+    # rstrip([chars]) : 인자로 전달된 문자를 String의 왼쪽에서 제거합니다.
+    # lstrip([chars]) : 인자로 전달된 문자를 String의 오른쪽에서 제거합니다.
     return len(str(number)) - len(str(number).lstrip('0'))
 
 
