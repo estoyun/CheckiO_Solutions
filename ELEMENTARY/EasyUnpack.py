62b5452dad93cdad17f42a5400f7f81960195004
PyeongYun Myeong <audvuddbs@gmail.com>
2020-08-19 01:28:32 +0900

    Update EasyUnpack.py
    문체 통일

--- ELEMENTARY/EasyUnpack.py
+++ ELEMENTARY/EasyUnpack.py
@@ -1,6 +1,6 @@
 # "리스트"는 [ ]으로 둘러싸지만, "튜플"은 ( )으로 둘러싼다.
 # "리스트"는 그 값의 생성, 삭제, 수정이 가능하지만 "튜플"은 그 값을 바꿀 수 없다.
-# "튜플"은 변경이 불가능하기 때문에 처리 속도가 빠름
+# "튜플"은 변경이 불가능하기 때문에 처리 속도가 빠르다.
 def easy_unpack(elements: tuple) -> tuple:
     """
         returns a tuple with 3 elements - first, third and second to the last
