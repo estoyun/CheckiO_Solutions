0458c1503d842129b5ba7c434389e46ba1e425ea
PyeongYun Myeong <estoyun@kakao.com>
2020-09-01 12:09:10 +0900

    Update EvenTheLast.py

--- ELEMENTARY/EvenTheLast.py
+++ ELEMENTARY/EvenTheLast.py
@@ -3,6 +3,7 @@
 def checkio(array: list) -> int:
     """
         sums even-indexes elements and multiply at the last
+        0을 포함한 짝수 인덱스의 값들의 합과 리스트 맨 마지막 값의 곱 연산 결과 출력
     """
     return 0 if array==[] else sum(array[i] for i in range(0,len(array),2))*array[len(array)-1]
 
